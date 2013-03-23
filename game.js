84c006cdfbe0f6dd707c6dae4b59feec75c7b5b1
Emil Österlund <emil@larsemil.se>
2013-03-24 00:34:33 +0100

    Working!

--- game.js
+++ game.js
@@ -3,33 +3,7 @@
 var GRID_WIDTH = 960;
 var GRID_HEIGHT = 640;
 var BLOCK_SIZE = 64;
-var map = {
-	world_width: 15,
-	world_height:10,
-	world_tiles:
-			[
-				[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,0,0,1,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,1,1,1,1,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-				[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
-				[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-
-			],
-	players:
-		[
-			{pos_x: 2, pos_y: 2}
-
-
-
-
-		]
-}
-
+map = [];
 isRunning = true; 
 
 // WebSockets
@@ -41,6 +15,7 @@ function connect() {
 
 	sock.onopen = function() {
 		console.log("connected to " + wsuri);
+		GameEngine.init(); 
 	}
  
 	sock.onclose = function(e) {
@@ -51,7 +26,16 @@ function connect() {
 	}
  
 	sock.onmessage = function(e) {
-		console.log("message received: " + e.data);
+		
+		try
+		{
+			map = JSON.parse(e.data);
+		}
+		catch(error)
+		{
+			console.log("funkänt");
+		}
+			GameEngine.loop();
 	}
 };
 
@@ -68,7 +52,6 @@ var GameEngine =  {
 
 	//methods
 	init : function(){
-		connect();
 		this.activeCanvas = document.getElementById('zero');
 		this.inactiveCanvas = document.getElementById('one');
 
@@ -84,8 +67,15 @@ var GameEngine =  {
 		evarglice.bind('keydown',[document.body],GameEngine.keyDown);
 		evarglice.bind('keyup',[document.body],GameEngine.keyUp);
 
+		this.keyIsPressed = [];
+		this.keyIsPressed[32] = 0;
+		this.keyIsPressed[37] = 0;
+		this.keyIsPressed[38] = 0;
+		this.keyIsPressed[39] = 0;
+		this.keyIsPressed[40] = 0;
 
-		window.setInterval(this.loop,10); 
+
+//		window.setInterval(this.loop,10); 
 
 		return true;
 	},
@@ -94,24 +84,29 @@ var GameEngine =  {
 		
 		if(data.keyCode == 32 || data.keyCode == 37 || data.keyCode == 38 || data.keyCode == 39 || data.keyCode == 40)
 		{
-			console.log(data);
-			keyPress = {type: 'press',keyCode:data.keyCode};
-			console.log(keyPress);
-			
-			//send keypress to server
-			send(JSON.stringify(keyPress));
-			event.preventDefault();
+			if(!GameEngine.keyIsPressed[data.keyCode])
+			{
+				keyPress = {type: 'pressed',keyCode:data.keyCode};
+
+				//send keypress to server
+				send(JSON.stringify(keyPress));
+
+				GameEngine.keyIsPressed[data.keyCode] = 1;
+			}
+		event.preventDefault();
 		}
 	},
 	keyUp: function(data){
 		if(data.keyCode == 32 || data.keyCode == 37 || data.keyCode == 38 || data.keyCode == 39 || data.keyCode == 40){
-			keyPress = {type: 'release',keyCode:data.keyCode};
-			console.log(keyPress);
+			keyPress = {type: 'released',keyCode:data.keyCode};
 			send(JSON.stringify(keyPress));
+
 			event.preventDefault();
+			GameEngine.keyIsPressed[data.keyCode] = 0; 
 			return false;
 		}
 	},
+
 	flip : function(){
 			tmp = this.activeCanvas;
 			this.activeCanvas = this.inactiveCanvas;
@@ -131,28 +126,31 @@ var GameEngine =  {
 
 	draw: function(){
 		this.ctx = this.inactiveCanvas.getContext("2d");
-
-		//Draw the world
-		for(y = 0; y < map.world_height; y++)
+		this.ctx.clearRect(0,0,GRID_WIDTH,GRID_HEIGHT);
+		if(typeof map == 'object')
 		{
+			//Draw the world
 			for(x = 0; x < map.world_width; x++)
 			{
-				if(map.world_tiles[y][x])
-					this.ctx.drawImage(this.sprite,0,0,BLOCK_SIZE,BLOCK_SIZE, x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE); 
+				for(y = 0; y < map.world_height; y++)
+				{
+					if(map.world_tiles[x][y])
+						this.ctx.drawImage(this.sprite,0,0,BLOCK_SIZE,BLOCK_SIZE, x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE); 
+				}
+
 			}
 
+			//Draw the players
+			for(c = 0; c<map.players.length; c++)
+			{	
+				this.ctx.drawImage(this.sprite,2*BLOCK_SIZE, 0, BLOCK_SIZE, BLOCK_SIZE, map.players[c].pos_x * BLOCK_SIZE, map.players[c].pos_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+			}
+		this.flip();
 		}
 		
-		//Draw the players
-		for(c = 0; c<map.players.length; c++)
-		{
-			this.ctx.drawImage(this.sprite,2*BLOCK_SIZE, 0, BLOCK_SIZE, BLOCK_SIZE, map.players[c].pos_x * BLOCK_SIZE, map.players[c].pos_y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-
-		}
-
-		this.flip();
 	}
 
 }
 
-window.onload = GameEngine.init(); 
+connect();
